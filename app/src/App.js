ac4f9ec65cc61b1415167fef60cf81cc5f9ac51e
BenPuusta <benpuusta@gmail.com>
2023-03-16 19:15:39 +0000

    added default not found layout if planet doesn't exist in db

--- app/src/App.js
+++ app/src/App.js
@@ -13,61 +13,72 @@ const landingPage = [
       "Earth has been around for quite some time. By researching Earths rocks, scientists have calculated the Earth to be around 4.5 billion years old!",
   },
 ];
+const errorPage = [
+  {
+    planet_image:
+      "https://imageio.forbes.com/specials-images/imageserve/5ed6636cdd5d320006caf841/The-Blackout-Tuesday-movement-is-causing-Instagram-feeds-to-turn-black-/960x0.jpg?format=jpg&width=960",
+    planet_name: "Planet Not Found",
+    planet_radius: 0,
+    planet_random_fact: "Try a real planet",
+  },
+];
 
 function App() {
   const [planets, setPlanetState] = useState(landingPage);
-  const [searchTerm, setSearchTerm] = useState("")
-
+  const [searchTerm, setSearchTerm] = useState("");
 
   async function fetchPlanetData() {
     const response = await fetch(
       "https://planet-rest-api-backend.onrender.com/random"
     );
     let data = await response.json();
+    console.log(data.payload);
+
     setPlanetState(data.payload);
-    console.log("clicked");
-    console.log(planets);
   }
 
-async function handleSearch(event) {
-  console.log ("Started search")
- event.preventDefault();
- const response = await fetch(`https://planet-rest-api-backend.onrender.com/search?planet=${searchTerm}`) ;
-  let data = await response.json();
-  setPlanetState(data.payload);
-  console.log(data.payload)
-}
+  async function handleSearch(event) {
+    console.log("Started search");
+    event.preventDefault();
+    const response = await fetch(
+      `https://planet-rest-api-backend.onrender.com/search?planet=${searchTerm}`
+    );
+    let data = await response.json();
 
-  console.log("Planets:", planets);
-  //console.log(event.target.value)
+    if (data.payload[0] === undefined) {
+      setPlanetState(errorPage);
+    } else {
+      setPlanetState(data.payload);
+    }
+  }
 
   return (
     <div>
       <div id="SearchCard">
         <h1 id="ApiTitle">Planets.REST</h1>
         <form onSubmit={handleSearch}>
-        <input
-          className="search-box"
-          type="text"
-          placeholder="Search.."
-          name="search"
-          onChange={(event)=>setSearchTerm(event.target.value)}
-          
-        ></input> 
-        <button className="button-search">Search</button>
-        <div id="Button-Container">
-          <button onClick={fetchPlanetData} className="Next-button" type="submit">
-            Random Planet
-          </button>
-          
-        </div>
+          <input
+            className="search-box"
+            type="text"
+            placeholder="Search.."
+            name="search"
+            onChange={(event) => setSearchTerm(event.target.value)}></input>
+          <button className="button-search">Search</button>
+          <div id="Button-Container">
+            <button
+              onClick={fetchPlanetData}
+              className="Next-button"
+              type="submit">
+              Random Planet
+            </button>
+          </div>
         </form>
       </div>
       <div id="ApiCard">
         <div className="right-pannel">
           <h1 id="PlanetHere"> {`${planets[0].planet_name}`}</h1>
           <div className="data-wrapper">
-            <p id="RadiusHere">{`${planets[0].planet_radius}`} km</p>
+            <p id="RadiusHere"> Radius: {`${planets[0].planet_radius}`} km</p>
           </div>
           <div className="data-wrapper">
             <p id="FactHere">{`${planets[0].planet_random_fact}`}</p>
@@ -77,8 +88,7 @@ async function handleSearch(event) {
           <img
             id="PlanetIMG"
             alt="Chosen planet"
-            src={`${planets[0].planet_image}`}
-          ></img>
+            src={`${planets[0].planet_image}`}></img>
         </div>
       </div>
     </div>
